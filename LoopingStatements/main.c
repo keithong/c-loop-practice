db6488a102c08475b5a2ad78278ce9cf35a396b4
Keith Samson <k.samson@klab.com>
2014-06-04 10:36:55 +0800

    First commit for Looping Statements

---
+++ LoopingStatements/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  LoopingStatements
+//
+//  Created by Keith Samson on 6/4/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    printf("This is a looping statement practice in C!\n");
+    return 0;
+}
+
